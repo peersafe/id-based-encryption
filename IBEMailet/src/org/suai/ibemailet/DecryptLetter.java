2438a5bf25cef0831f8dd7becbdbb934dda3887f
foxneig <foxneig@gmail.com>
2011-05-03 17:49:50 +0000

    Ciphertext is packed into Base64. Now it works =))

--- IBEMailet/src/org/suai/ibemailet/DecryptLetter.java
+++ IBEMailet/src/org/suai/ibemailet/DecryptLetter.java
@@ -2,20 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.suai.ibemailet;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
-import java.security.Security;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.logging.Level;
@@ -38,14 +34,15 @@ import org.suai.idbased.Client;
 import org.suai.idbased.DecryptException;
 import org.suai.idbased.PKG;
 import org.suai.idbased.Util;
-import org.apache.james.security.InitJCE;
-import org.suai.idbased.Cryptocontainer;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
  *
  * @author foxneig
  */
 public class DecryptLetter extends GenericMailet {
+
     private MailetConfig config;
     private String mpk_path;
     private String msk1_path;
@@ -53,12 +50,11 @@ public class DecryptLetter extends GenericMailet {
     private PKG pkg;
     private Client client;
 
-
     @Override
-    public void destroy () {
-        System.out.println ("Destroy");
+    public void destroy() {
+        System.out.println("Destroy");
 
-        }
+    }
 
     @Override
     public String getMailetInfo() {
@@ -73,21 +69,13 @@ public class DecryptLetter extends GenericMailet {
     @Override
     public void init(MailetConfig config) throws MessagingException {
 
-            System.out.println("Init IdBasedDecryptMailet");
-
-
-
-
-
-
-
-
-            super.init(config);
-            MailetContext context = config.getMailetContext();
-            mpk_path = getInitParameter("mpkPath");
-            msk1_path = getInitParameter("msk1Path");
-            msk2_path = getInitParameter("msk2Path");
-            pkg = new PKG();
+        System.out.println("Init IdBasedDecryptMailet");
+        super.init(config);
+        MailetContext context = config.getMailetContext();
+        mpk_path = getInitParameter("mpkPath");
+        msk1_path = getInitParameter("msk1Path");
+        msk2_path = getInitParameter("msk2Path");
+        pkg = new PKG();
         try {
             pkg.MPK = Util.readKeyData(new FileInputStream(mpk_path));
         } catch (IOException ex) {
@@ -103,9 +91,35 @@ public class DecryptLetter extends GenericMailet {
         } catch (IOException ex) {
             Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
         }
-            pkg.getSecretExponent();
+        pkg.getSecretExponent();
 
 
+
+
+
+    }
+
+    public byte[] getAttachments(InputStream is) {
+
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buff = new byte[8];
+        int i = 0;
+        do {
+            try {
+                i = is.read(buff);
+                bos.write(buff);
+            } catch (IOException ex) {
+                log("Cannot read from attaches");
+            }
+
+        } while (i != -1);
+        try {
+            is.close();
+        } catch (IOException ex) {
+            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bos.toByteArray();
 
 
 
@@ -114,40 +128,40 @@ public class DecryptLetter extends GenericMailet {
     @Override
     public void service(Mail mail) throws MessagingException {
         client = new Client();
-        byte [] decrypted = null;
+        byte[] decrypted = null;
         InputStream is = null;
-        FileOutputStream fout = null;
-        try {
-           fout = new FileOutputStream("/home/foxneig/mailbody");
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Multipart mp = null;
+        byte[] body = null;
+        ByteArrayInputStream bin = null;
+        String text = null;
+        BASE64Encoder enc = new BASE64Encoder();
+        BASE64Decoder dec = new BASE64Decoder();
         MimeMessage message = mail.getMessage();
         String contentType = message.getContentType();
-        System.out.println (contentType);
+        System.out.println(contentType);
         MailAddress from = mail.getSender();
         Collection to = mail.getRecipients();
         Iterator<MailAddress> iterator = to.iterator();
         String recip = iterator.next().toString();
         String sender = from.toString();
-        System.out.println ("E-mail FROM: " + sender);
-        System.out.println ("E-mail TO: "+recip);
+        System.out.println("E-mail FROM: " + sender);
+        System.out.println("E-mail TO: " + recip);
         if (message.isMimeType("text/plain")) {
             try {
-                String text = (String) message.getContent();
-                is = new ByteArrayInputStream (text.getBytes());
-
-                message.writeTo(fout);
+                text = (String) message.getContent();
             } catch (IOException ex) {
                 Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
             }
-          
-
-
-                System.out.println ("Decrypt mail body...");
+            try {
+                body = dec.decodeBuffer(text);
+            } catch (IOException ex) {
+                Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            bin = new ByteArrayInputStream(body);
+            System.out.println("Decrypt mail body...");
             try {
                 try {
-                    decrypted = client.decryptData(is, recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
+                    decrypted = client.decryptData(bin, recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
                 } catch (FileNotFoundException ex) {
                     Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (DecryptException ex) {
@@ -166,112 +180,114 @@ public class DecryptLetter extends GenericMailet {
             } catch (BadPaddingException ex) {
                 Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
             }
-                System.out.println ("Done");
-                String plaintext = new String (decrypted);
-               // System.out.println (plaintext);
-
-                message.setContent(plaintext, contentType);
-                message.setHeader(RFC2822Headers.CONTENT_TYPE, contentType);
-                message.saveChanges();
+            System.out.println("Done");
+            String plaintext = new String(decrypted);
+            message.setContent(plaintext, contentType);
+            message.setHeader(RFC2822Headers.CONTENT_TYPE, contentType);
+            message.saveChanges();
 
 
-        }
-        else if (message.isMimeType("multipart/mixed") ||message.isMimeType("multipart/related")  ) {
+        } else if (message.isMimeType("multipart/mixed") || message.isMimeType("multipart/related") || message.isMimeType("multipart/alternative")) {
 
             try {
-                // здесь надо сохранить аттачи
-                Multipart mp = (Multipart) message.getContent();
+                mp = (Multipart) message.getContent();
+            } catch (IOException ex) {
+                Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
-                System.out.println ("PartsNum: "+mp.getCount());
-
-                for (int i = 0, n = mp.getCount(); i < n; i++) {
-                    Part part = mp.getBodyPart(i);
-
-                    if (part.isMimeType("text/plain")) {
-//                     System.out.println ("Try to decrypt text");
-//                        try {
-//                            try {
-//                                decrypted = client.decryptData(part.getInputStream(), recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
-//                            } catch (FileNotFoundException ex) {
-//                                Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                            } catch (DecryptException ex) {
-//                                Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                            }
-//                        } catch (NoSuchAlgorithmException ex) {
-//                            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (NoSuchPaddingException ex) {
-//                            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (InvalidKeyException ex) {
-//                            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (IllegalBlockSizeException ex) {
-//                            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (BadPaddingException ex) {
-//                            Logger.getLogger(EncryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-//                        }
-//                     part.setContent(new String(decrypted), part.getContentType());
-//                     boolean removeBodyPart = mp.removeBodyPart((BodyPart) part);
-//                     System.out.println ("Removed: "+removeBodyPart);
-//                     mp.addBodyPart((BodyPart) part,i);
-//                     message.setContent(mp);
-//
-//
-
-                        ;
+            for (int i = 0, n = mp.getCount(); i < n; i++) {
+                Part part = mp.getBodyPart(i);
+                if (part.isMimeType("text/plain")) {
+                    System.out.println("Try to decrypt text");
+                    try {
+                        text = (String) part.getContent();
+                    } catch (IOException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    else {
+                    try {
+                        body = dec.decodeBuffer(text);
+                    } catch (IOException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    bin = new ByteArrayInputStream(body);
+                    try {
+                        decrypted = client.decryptData(bin, recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NoSuchAlgorithmException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NoSuchPaddingException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvalidKeyException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalBlockSizeException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (BadPaddingException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (DecryptException ex) {
+                        Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    part.setContent(new String(decrypted), part.getContentType());
+                    mp.removeBodyPart((BodyPart) part);
+                    mp.addBodyPart((BodyPart) part, i);
+                    message.setContent(mp);
+                    message.saveChanges();
+
+                } else {
 
                     String disposition = part.getDisposition();
-                    System.out.println ("Disposition "+disposition);
                     if ((disposition != null) && ((disposition.equals(Part.ATTACHMENT) || (disposition.equals(Part.INLINE))))) {
+                        try {
+                            text = (String) part.getContent();
+                        } catch (IOException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        try {
+                            body = dec.decodeBuffer(text);
+                        } catch (IOException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        bin = new ByteArrayInputStream(body);
+                        System.out.println("Try to decrypt attache");
+                        try {
+                            decrypted = client.decryptData(bin, recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
+                        } catch (FileNotFoundException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IOException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (NoSuchAlgorithmException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (NoSuchPaddingException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (InvalidKeyException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IllegalBlockSizeException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (BadPaddingException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (DecryptException ex) {
+                            Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
+                        }
 
-                        System.out.println ("Try to decrypt attache");
-                            try {
-                                try {
-                                    decrypted = client.decryptData(part.getInputStream(), recip, sender, pkg.keyExtract(recip), pkg.MPK, pkg.e);
-                                } catch (FileNotFoundException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (NoSuchPaddingException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (InvalidKeyException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (IllegalBlockSizeException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (BadPaddingException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                } catch (DecryptException ex) {
-                                    Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-                            } catch (NoSuchAlgorithmException ex) {
-                                Logger.getLogger(DecryptLetter.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                             System.out.println ("decrypted is set");
-                             
                         part.setContent(decrypted, part.getContentType());
-                        part.setFileName("decrypted"+i);
-                        boolean removeBodyPart = mp.removeBodyPart((BodyPart) part);
-                        System.out.println ("Removed: "+removeBodyPart);
-                        mp.addBodyPart((BodyPart) part,i);
-
+                        mp.removeBodyPart((BodyPart) part);
+                        mp.addBodyPart((BodyPart) part, i);
                         message.setContent(mp);
+                        message.saveChanges();
+                        System.out.println("Attache is decrypted");
 
 
 
-
-
-                     System.out.println ("Attache is decrypted");
-
-
-
-                    }
                     }
                 }
-            } catch (IOException ex) {
-               log("Cannot to get attaches");
             }
+
         }
         message.setHeader(RFC2822Headers.CONTENT_TYPE, contentType);
-        message.saveChanges();
-        System.out.println ("Ended");
+
+        System.out.println("Ended");
 
 
 
@@ -279,6 +295,4 @@ public class DecryptLetter extends GenericMailet {
 
 
     }
-
-
 }
