dfc0b6449691c2963f0a5d3ea857c84544c3f1fd
foxneig@gmail.com <foxneig@gmail.com@ec0b9d82-9075-60f8-2e66-2effe97479cc>
2011-05-11 12:27:03 +0000

    Added new class for working with KeyStorage

---
+++ id_based_encrypt_2803/src/org/suai/idbased/Domain.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.suai.idbased;
+
+/**
+ *
+ * @author foxneig
+ */
+public class Domain {
+    private String domainName;
+    private String MPK;
+    private String MSK1;
+    private String MSK2;
+    private String checksumm1;
+    private String checksumm2;
+
+    public void set (String dn, String MPK, String MSK1, String MSK2, String checksumm1, String checksumm2) {
+        this.domainName = dn;
+        this.MPK = MPK;
+        this.MSK1 = MSK1;
+        this.MSK2 = MSK2;
+        this.checksumm1 = checksumm1;
+        this.checksumm2 = checksumm2;
+    }
+    public String getMPK () {
+        return this.MPK;
+    }
+    public String getMSK1 () {
+        return this.MSK1;
+    }
+    public String getMSK2 () {
+        return this.MSK2;
+    }
+    public String getName () {
+        return this.domainName;
+    }
+    public String getMSK1CheckSum () {
+        return this.checksumm1;
+    }
+      public String getMSK2CheckSum () {
+        return this.checksumm2;
+    }
+
+}
